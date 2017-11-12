ade8fc346a458da566e333fe1fd0313d177b17b1
TimoGo <t.gohde@posteo.de>
2017-11-12 09:53:06 +0100

    script1.sh
    modify /boot

---
+++ script1.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+if [[ $# = 0 ]]; then
+ echo "invoke with mountpoint as first parameter eg:  $0 /mnt"
+ exit 1
+fi
+# verbosemode on
+set -v
+test -f $1/config.txt || exit
+cp $1/config.txt $1/config.txt.sik
+touch $1/ssh
+# append in lines with "console=" in it
+sed -i.sik '/console=/ s/$/ modules-load=dwc2,g_ether/' cmdline.txt
+echo "dtoverlay=dwc2" >> $1/config.txt
